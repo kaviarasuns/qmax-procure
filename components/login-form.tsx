905b1cb4403c536d9e8ed39f73c1b1d2cf703fc3
Kaviarasu S <kaviarasus@Kaviarasus-Mac-mini.local>
2025-08-09 13:55:20 +0530

    eslint fixes

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -4,16 +4,9 @@ import { cn } from "@/lib/utils";
 import { createClient } from "@/lib/supabase/client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
